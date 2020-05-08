0a84642c361b8fe4933780361b95bbd40e14bbb2
its-nedum <de.magnificent1@gmail.com>
2020-05-08 01:15:45 +0100

    features(still implementing design)

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../../styles/about.css'
 
 function About() {
     return (
         <div>
-            <h1>About Page</h1>
+            <div className="first_container">
+                <div className="first_banner">
+                
+                </div>
+            </div>
+            <div className="second_container">
+                <div className="second_banner">
+                
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -5,8 +5,8 @@ function Home() {
     return (
         <div className="banner_container">
             <div className="banner">
-               <h1>Hi, I'm Chinedu Emesue</h1>
-               <h1>Full Stack Web Developer</h1>
+               <h1 className="intro_name">Hi, I'm Chinedu Emesue</h1>
+               <p className="intro_info">Full Stack Web Developer, IT Engineer and TV Series Lover.</p>
             </div>
         </div>
     )
