35f482a8ece86b59e4acd1804f5b77ffba82659e
its-nedum <de.magnificent1@gmail.com>
2020-05-19 15:41:37 +0100

    chore(started contact form submit)

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,14 +1,44 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState } from 'react'
 import Footer from '../footer/footer'
 import '../../styles/contact.css'
+import axios from 'axios'
 
 function Contact() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [inputs, setInputs] = useState({
+        name: "", 
+        email: "",
+        message: ""
+    })
 
-    const validateName = (e) => {
-        console.log("Hello world")
+    const handleSubmit = (e) => {
+        e.preventDefault();        
+        if(!name && !email && !message){
+            return 
+        }else{
+            setInputs({
+                name,
+                email,
+                message
+            })
+            setName(''); 
+            setEmail(); 
+            setMessage();
+            console.log(inputs)
+            axios({
+                method: "POST",
+                url: "https://formspree.io/emesuechinedu@gmail.com",
+                data: inputs
+            })
+            .then(r => {
+                console.log("Thanks!");
+              })
+            .catch(r => {
+                console.log("Error");
+            });
+        }
     }
     
     return (
@@ -27,18 +57,18 @@ function Contact() {
                         <a href="https://www.facebook.com/itsnedum" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
                     </div>
                     </div>
-                    <form className="col-sm-12 col-md-6">
+                    <form className="col-sm-12 col-md-6" onSubmit={handleSubmit}>
                         <div className="row">
                             <div className="col-sm-12 col-md-6">
                                 <div className="form-group mb-3">
                                     <label htmlFor="fullname">Full Name</label>
-                                    <input type="text" id="fullname" className="form-control" placeholder="John Doe" onChange={validateName()} required=""/>
+                                    <input type="text" id="fullname" className="form-control" placeholder="John Doe" value={name} onChange={e => setName(e.target.value)} required=""/>
                                 </div>
                             </div>
                             <div className="col-sm-12 col-md-6">
                                 <div className="form-group mb-3">
                                     <label htmlFor="email">E-mail</label>
-                                    <input type="email" id="email" className="form-control" placeholder="john.doe@gmail.com" required=""/>
+                                    <input type="email" id="email" className="form-control" placeholder="john.doe@gmail.com" value={email} onChange={e => setEmail(e.target.value)} required=""/>
                                 </div>
                             </div>
                         </div>
@@ -46,13 +76,13 @@ function Contact() {
                             <div className="col-sm-12">
                                 <div className="form-group">
                                     <label htmlFor="message">Message</label>
-                                    <textarea className="form-control" id="message" rows="8" placeholder="Hello..." required=""></textarea>
+                                    <textarea className="form-control" id="message" rows="8" placeholder="Hello..." value={message} onChange={e => setMessage(e.target.value)} required=""></textarea>
                                 </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="submit_btn">
-                                <input type="submit" className="form-control btn btn-primary" value="Submit" />
+                                <input type="submit" className="form-control btn btn-primary" value="Submit" onClick={handleSubmit} />
                             </div>
                         </div>
                     </form>
